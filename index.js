0af16e0ca628a19b7fc5eec5ad9948c5dc14309f
Boris <borismjimenezj@gmail.com>
2024-09-17 23:43:43 -0400

    incluye codigo de desencriptado

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const $textarea = document.querySelector(".form-textarea").value;
+
+const $button_encriptar = document.querySelector("button");
+
+console.log($textarea);
+console.log($button_encriptar);
+
 function encriptador(e) {
   if (e === "e") {
     return (e = "enter");
@@ -23,10 +30,29 @@ const encriptar = (text) => {
 
 console.log(encriptar("hola como esTas "));
 
+function desencriptador(e) {
+  if (
+    e.includes("enter") === true ||
+    e.includes("imes") === true ||
+    e.includes("ai") === true ||
+    e.includes("ober") === true ||
+    e.includes("ufat") === true
+  ) {
+    let letra_e = e.replaceAll("enter", "e");
+    let letra_i = letra_e.replaceAll("imes", "i");
+    let letra_a = letra_i.replaceAll("ai", "a");
+    let letra_o = letra_a.replaceAll("ober", "o");
+    let letra_u = letra_o.replaceAll("utaf", "u");
+    return letra_u;
+  }
+}
 const desencriptar = (text) => {
   let texto = text.toLowerCase().trim();
-  let texto_modificado = texto.match("enter");
-  return texto_modificado;
+  let texto_modificado = texto.split(" ");
+  let texto_nuevo = texto_modificado.map(desencriptador);
+  let texto_ultimo = texto_nuevo.join(" ");
+
+  return texto_ultimo;
 };
 
 console.log(desencriptar("hoberlai cobermober enterstais"));
